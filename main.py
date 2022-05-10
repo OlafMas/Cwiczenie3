5455c6f14505dc20bc897b62736fa6516c82cef6
OlafMas <Kiepscy97!>
2022-05-10 21:04:11 +0200

    Zadanie3

--- main.py
+++ main.py
@@ -1,76 +1,55 @@
 from PIL import Image
 from pathlib import Path
 import numpy as np
-from skimage.feature import graycomatrix,graycoprops
+from skimage.feature import greycomatrix, greycoprops
 import csv
+
+
 
 patch_size = 128
 dataset=[]
+angs=[0, 48, 90, 135]
+dists=[1,3,5]
+feature_names = ['dissimilarity', 'contrast', 'correlation']
 
-
-    classes = ['panele', 'tynk', 'laminat']
-    root = Path(R,'C:\Users\Olaf\Tek')
-    out_dir= root/'patches'
-    for klasa in classes:
-        klasa_path=root / klasa
-        for filepath in klasa_path.iterdir():
-            image = Image.open(str(filepath))
-            for i in range(10):
-                xo=np.random.randint(image.width)
-                yo=np.random.randint(image.height)
-
-                patch=Image.crop(xo,yo,patch_size,patch_size)
-                patch.save(out_dir/klasa/filepath.stem,+,f'_{i:o2d})
-                patch_gs=patch.convert('L')
-                features=get_glcm(patch)
-                features +=[klasa]
-                dataset +=[features]
-
-            header=['', '', 'category'] #wygenerowac cechy
-            with open('output_data.csv','w',newline='')as out_csv:
-                 csvwriter=csv.writer(out_csv,delimiter='\t')
-                 csvwriter.writerow(header)
-                 csvwriter.writerows(dataset)
-
-                 feature_names=['dissimilrity','contrast','correlation']
-                 angs=[0, 48, 90, 135]
-                 dists=[1,3,5]
-
-
-def get_glcm(patch: Image)->List[float]:
+def get_glcm(patch: Image)->list[float]:
     patch_64 = np.array(patch)
-    patch_64 /= np.max(patch_64)
+    patch_64 = patch_64 / np.max(patch_64)
     patch_64 *= 63
-    patch_64 = patch_64.astype(np.unit8)
-    graycomatrix(patch_64,dists,angs,64,True,True)
-    feature_vector = []:
-    for feature in features:
-        data=graycoprops(glcm,feature).flatten()
-        feature_vector.extends[data.tolist()]
+    patch_64 = patch_64.astype(np.uint8)
+    glcm = greycomatrix(patch_64,dists,angs,64,True,True)
+    feature_vector = []
+    for feature in feature_names:
+        data = greycoprops(glcm,feature).flatten()
+        feature_vector.extend(data.tolist())
     return feature_vector
 
-'''
-features = crop_and_get_features([128,128])
+classes = ['panele', 'tynk', 'laminat']
+root = Path(R'C:\Users\Olaf\Tek')
+out_dir= root/'patches'
+for klasa in classes:
+    klasa_path=root / klasa
+    for filepath in klasa_path.iterdir():
+        image = Image.open(str(filepath))
+        for i in range(10):
+            xo=np.random.randint(image.width)
+            yo=np.random.randint(image.height)
 
-full_features_names=get_full_names()
-full_features_names.append('Category')
+            patch=image.crop((xo,yo,xo+patch_size,yo+patch_size))
+            # patch.save(out_dir/klasa/f'{filepath.stem}_{i:02d}.jpg')
+            patch_gs=patch.convert('L')
+            features=get_glcm(patch_gs)
+            features +=[klasa]
+            dataset +=[features]
 
-df=DataFrame(data=features,columns=full_features_names)
-df.to_csv('textures_data2.csv',sep=',',index=False)
+        header=['', '', 'category'] #wygenerowac cechy
+        with open('output_data.csv','w',newline='')as out_csv:
+             csvwriter=csv.writer(out_csv,delimiter='\t')
+             csvwriter.writerow(header)
+             csvwriter.writerows(dataset)
 
-from sklearn import svm
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 
-classifier=svm.SVC()
-data=np.array(features)
-X = data[:,:-1]
-Y = data[:,-1]
 
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.33)
 
-classifier.fit(x_train,y_train)
-y_pred = classifier.predict(x_test)
-acc = accuracy_score(y_test,y_pred)
-print(acc)
-'''
+
+
